fc625d16e3454219be5faa55562c308850467fed
Gartman Stepan <sgartman122881@gmail.com>
2023-10-07 22:09:32 +0700

    Добавьте файлы проекта.

---
+++ sem3.lab2/sem3.lab2.cpp
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include <windows.h>
+#include "Lib.h"
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    SetConsoleCP(1251);
+
+
+
+
+
+}
