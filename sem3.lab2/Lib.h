d94b30c8c011597231628c6c1eaad8f81366b23b
Gartman Stepan <sgartman122881@gmail.com>
2023-10-08 01:02:02 +0700

    UTF-8

--- sem3.lab2/Lib.h
+++ sem3.lab2/Lib.h
@@ -1,4 +1,4 @@
-#pragma once
+п»ї#pragma once
 #define Len 30
 #define LenDate 10
 #include <iostream>
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <conio.h>
 
-// Структуры
+// РЎС‚СЂСѓРєС‚СѓСЂС‹
 
 struct Author {
     char* name;
@@ -40,9 +40,9 @@ struct Library {
     char* address;
 };
 
-// Вспомогательные функции
+// Р’СЃРїРѕРјРѕРіР°С‚РµР»СЊРЅС‹Рµ С„СѓРЅРєС†РёРё
 
-char vvod(char min, char max) { // автоматизировал проверку на допустимость ввода, ввод без нажатия ENTER
+char vvod(char min, char max) { // Р°РІС‚РѕРјР°С‚РёР·РёСЂРѕРІР°Р» РїСЂРѕРІРµСЂРєСѓ РЅР° РґРѕРїСѓСЃС‚РёРјРѕСЃС‚СЊ РІРІРѕРґР°, РІРІРѕРґ Р±РµР· РЅР°Р¶Р°С‚РёСЏ ENTER
     char x;
     do {
         x = _getch();
@@ -54,20 +54,20 @@ void wait() {
     while (getchar() != '\n');
 }
 
-// Основные функции
+// РћСЃРЅРѕРІРЅС‹Рµ С„СѓРЅРєС†РёРё
 
-Author AuthorInit(char* name, char* date, char* country) { // инициализация автора
+Author AuthorInit(char* name, char* date, char* country) { // РёРЅРёС†РёР°Р»РёР·Р°С†РёСЏ Р°РІС‚РѕСЂР°
     Author buf;
     if (strlen(name) == 0 || strlen(country) == 0) {
         exit(-1);
     }
     else if (strlen(date) != LenDate) {
-        puts("Я тупой прост немножко");
+        puts("РЇ С‚СѓРїРѕР№ РїСЂРѕСЃС‚ РЅРµРјРЅРѕР¶РєРѕ");
         exit(-1);
         }
         else {
             if (date[0] < '0' || date[0] > '3' || date[1] < '0' || (date[1] > '1' && date[0] > '2') || date[1] > '9' || date[2] != '.' || date[3] < '0' || date[3] > '1' || date[4] < '0' || (date[4] > '0' && date[3] > '2') || date[4] > '9' || date[5] != '.' || date[6] < '0' || date[6] > '9' || date[7] < '0' || date[7] > '9' || date[8] < '0' || date[8] > '9' || date[9] < '0' || date[9] > '9') {
-                puts("У тебя ошибка во времени, братан, так что извиняй, я пошёл");
+                puts("РЈ С‚РµР±СЏ РѕС€РёР±РєР° РІРѕ РІСЂРµРјРµРЅРё, Р±СЂР°С‚Р°РЅ, С‚Р°Рє С‡С‚Рѕ РёР·РІРёРЅСЏР№, СЏ РїРѕС€С‘Р»");
                 exit(-1);
             }
             else {
@@ -79,27 +79,27 @@ Author AuthorInit(char* name, char* date, char* country) { // инициализация авто
     return buf;
 } 
 
-Author AuthorInput() { // ввод автора
+Author AuthorInput() { // РІРІРѕРґ Р°РІС‚РѕСЂР°
     char* name = (char*)calloc(Len, sizeof(char));
     char* date = (char*)calloc(LenDate, sizeof(char));
     char* country = (char*)calloc(Len, sizeof(char));
 
-    puts("Введите имя автора");
+    puts("Р’РІРµРґРёС‚Рµ РёРјСЏ Р°РІС‚РѕСЂР°");
     gets_s(name, Len);
     do {
         do {
-            puts("Введите дату рождения автора в формате DD.MM.YYYY");
+            puts("Р’РІРµРґРёС‚Рµ РґР°С‚Сѓ СЂРѕР¶РґРµРЅРёСЏ Р°РІС‚РѕСЂР° РІ С„РѕСЂРјР°С‚Рµ DD.MM.YYYY");
             gets_s(date, 200);
         } while (date[0] < '0' || date[0] > '3' || date[1] < '0' || (date[1] > '1' && date[0] > '2') || date[1] > '9' || date[2] != '.' || date[3] < '0' || (date[3] == '1' && date[4] > '2') || date[3] > '1' || date[4] < '0' || (date[4] > '0' && date[3] > '2') || date[4] > '9' || date[5] != '.' || date[6] < '0' || date[6] > '9' || date[7] < '0' || date[7] > '9' || date[8] < '0' || date[8] > '9' || date[9] < '0' || date[9] > '9');
     } while (strlen(date) != 10);
-    puts("Введите страну происхождения автора");
+    puts("Р’РІРµРґРёС‚Рµ СЃС‚СЂР°РЅСѓ РїСЂРѕРёСЃС…РѕР¶РґРµРЅРёСЏ Р°РІС‚РѕСЂР°");
     gets_s(country, Len);
 
     Author buf = AuthorInit(name, date, country);
     return buf;
 } 
 
-void PrintAuthor(Author author) { // вывод информации об авторе
+void PrintAuthor(Author author) { // РІС‹РІРѕРґ РёРЅС„РѕСЂРјР°С†РёРё РѕР± Р°РІС‚РѕСЂРµ
     puts(author.name);
     puts(author.date);
     puts(author.country);
@@ -119,20 +119,20 @@ Client ClientInit(char* name, char* date, char* address) {
     return buf;
 }
 
-Client ClientInput() { // ввод клиента (читателя)
+Client ClientInput() { // РІРІРѕРґ РєР»РёРµРЅС‚Р° (С‡РёС‚Р°С‚РµР»СЏ)
     char* name = (char*)calloc(Len, sizeof(char));
     char* date = (char*)calloc(LenDate, sizeof(char));
     char* address = (char*)calloc(Len, sizeof(char));
 
-    puts("Введите имя читателя");
+    puts("Р’РІРµРґРёС‚Рµ РёРјСЏ С‡РёС‚Р°С‚РµР»СЏ");
     gets_s(name, Len);
     do {
         do {
-            puts("Введите дату рождения читателя в формате DD.MM.YYYY");
+            puts("Р’РІРµРґРёС‚Рµ РґР°С‚Сѓ СЂРѕР¶РґРµРЅРёСЏ С‡РёС‚Р°С‚РµР»СЏ РІ С„РѕСЂРјР°С‚Рµ DD.MM.YYYY");
             gets_s(date, 200);
         } while (date[0] < '0' || date[0] > '3' || date[1] < '0' || (date[1] > '1' && date[0] > '2') || date[1] > '9' || date[2] != '.' || date[3] < '0' || (date[3] == '1' && date[4] > '2') || date[3] > '1' || date[4] < '0' || (date[4] > '0' && date[3] > '2') || date[4] > '9' || date[5] != '.' || date[6] < '0' || date[6] > '9' || date[7] < '0' || date[7] > '9' || date[8] < '0' || date[8] > '9' || date[9] < '0' || date[9] > '9');
     } while (strlen(date) != 10);
-    puts("Введите адрес проживания читателя");
+    puts("Р’РІРµРґРёС‚Рµ Р°РґСЂРµСЃ РїСЂРѕР¶РёРІР°РЅРёСЏ С‡РёС‚Р°С‚РµР»СЏ");
     gets_s(address, Len);
 
     Client buf = ClientInit(name, date, address);
@@ -157,13 +157,13 @@ Book BookInput(Author author) {
     int year;
     char* name = (char*)calloc(Len, sizeof(char));
 
-    puts("Введите название книги");
+    puts("Р’РІРµРґРёС‚Рµ РЅР°Р·РІР°РЅРёРµ РєРЅРёРіРё");
     gets_s(name, Len);
-    puts("Введите год издания книги");
+    puts("Р’РІРµРґРёС‚Рµ РіРѕРґ РёР·РґР°РЅРёСЏ РєРЅРёРіРё");
     do {
         scanf("%d", &year);
         if (year < 1000) {
-            puts("А теперь нормально");
+            puts("Рђ С‚РµРїРµСЂСЊ РЅРѕСЂРјР°Р»СЊРЅРѕ");
         }
     } while (year < 1000);
 
@@ -175,9 +175,9 @@ Operation OperationInput() {
     char* move = (char*)calloc(Len, sizeof(char));
     char* date = (char*)calloc(LenDate, sizeof(char));
 
-    puts("Введите тип совершённой операции");
+    puts("Р’РІРµРґРёС‚Рµ С‚РёРї СЃРѕРІРµСЂС€С‘РЅРЅРѕР№ РѕРїРµСЂР°С†РёРё");
     gets_s(move, Len);
-    puts("Введите дату совершения операции");
+    puts("Р’РІРµРґРёС‚Рµ РґР°С‚Сѓ СЃРѕРІРµСЂС€РµРЅРёСЏ РѕРїРµСЂР°С†РёРё");
     gets_s(date, LenDate);
     Operation buf;
     return buf;
